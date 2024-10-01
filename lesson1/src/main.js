e61da4db6521e222a14eea2d4c2b6bc0403e826d
Hung <levuhung678@gmail.com>
2024-10-01 22:12:40 +0700

    Add new files and remove unused files

---
+++ lesson1/src/main.js
@@ -0,0 +1,12 @@
+//khai bao ten
+var name='giachoang';
+
+/*
+noi dung comment
+*/
+
+confirm('xac nhan du tuoi')
+prompt('xac nhan dutuoi')
+setInterval(function() {
+    console.log('day la log' + Math.random())
+},1000)
